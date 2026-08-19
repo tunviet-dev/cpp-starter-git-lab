2715a387a315affab82b84f31ae7304c6a32b7ab
Viet <levietxxx456@gmail.com>
2026-08-19 22:43:56 +0700

    feat(calc): add initial calculator API

---
+++ lab3/src/calculator.cpp
@@ -0,0 +1,5 @@
+#include "../include/calculator.hpp"
+
+int calculate(int a) {
+    return a * 2;
+}
---
+++ lab3/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/calculator.hpp"
+
+int main() {
+    std::cout << calculate(10) << std::endl;
+    return 0;
+}
+
